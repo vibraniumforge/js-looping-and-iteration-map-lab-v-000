f81b04073a2b4e0badbb5944a56269614501db84
Greg Morgan <gregm9999@gmail.com>
2019-01-09 03:36:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function(driver) {
+    return driver.toLowerCase();
+  })
+}
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    return `${driver.firstName}, ${driver.lastName}`;
+  })
+}
+
+function attributesToPhrase (driver) {
+
+}
